0cfed6c62f3eb3aec0c48049412bf90e6fc536f6
Joby-01 <Johan@PROBOOK>
2025-12-01 11:31:38 +0100

    listpage klar

--- AppWebRazor/Pages/ListPage.cshtml.cs
+++ AppWebRazor/Pages/ListPage.cshtml.cs
@@ -9,20 +9,79 @@ namespace AppWebRazor.Pages;
 
 public class ListPageModel : PageModel
 {
-    public List<IFriend> FriendsList {get;set;} = new List<IFriend>();
-    public List<IAddress> AddressesList {get;set;} = new List<IAddress>();
+
+    [BindProperty]
+    public string SelectedCountry { get; set; }
+
+    [BindProperty]
+    public string SelectedCity { get; set; }
+
+    public ResponsePageDto<IFriend> FriendsList { get; set; } = new ResponsePageDto<IFriend>();
+
+    public List<IAddress> AddressesList { get; set; } = new List<IAddress>();
+
+    public List<string> CityList { get; set; } = new List<string>();
 
     readonly IAddressesService _addressesService;
-    public ListPageModel(IAddressesService addressesService)
+    readonly IFriendsService _friendsService;
+
+    public ListPageModel(IAddressesService addressesService, IFriendsService friendsService)
     {
         _addressesService = addressesService;
+        _friendsService = friendsService;
     }
+
+    public async Task<IActionResult> OnPost()
+    {
+        await populatedAddressListAsync();
+        if (!string.IsNullOrWhiteSpace(SelectedCountry))
+        {
+            // Filter cities based on the selected country
+            CityList = AddressesList
+                .Where(a => a.Country == SelectedCountry)
+                .Select(a => a.City)
+                .Distinct()
+                .ToList();
+
+            // Reset city if country changed
+            if (SelectedCity != null && !CityList.Contains(SelectedCity))
+            {
+                SelectedCity = "";
+            }
+        }
+        else
+        {
+            CityList = new List<string>();
+            SelectedCity = "";
+        }
+        if (!string.IsNullOrWhiteSpace(SelectedCountry) && string.IsNullOrWhiteSpace(SelectedCity))
+        {
+            FriendsList = await _friendsService.ReadFriendsAsync(true, true, SelectedCountry.ToLower(), 0, 20);
+        }
+        else if (!string.IsNullOrWhiteSpace(SelectedCountry) && !string.IsNullOrWhiteSpace(SelectedCity))
+        {
+            FriendsList = await _friendsService.ReadFriendsAsync(true, true, SelectedCity.ToLower(), 0, 20);
+        }
+
+        return Page();
+    }
+
+    public void ToDetailsPage(Guid id)
+    {
+      Console.WriteLine(id);
+    }
+
     public async Task<IActionResult> OnGet()
     {
-        var x = await _addressesService.ReadAddressesAsync(true,false,null,0,10);
-        AddressesList = x.PageItems;
-        
+        await populatedAddressListAsync();
+
         return Page();
+    }
+
+    public async Task populatedAddressListAsync()
+    {
+        var x = await _addressesService.ReadAddressesAsync(true, false, null, 0, 10);
+        AddressesList = x.PageItems;
     }
 }
 
